2f9793cd9221887d613cb0f5f25fae4c6a5b8e91
AlixMcGill <alixfmcgill@gmail.com>
2026-09-21 21:32:49 -0500

    Lab done

--- Labs/Lab_Five/src/main.cpp
+++ Labs/Lab_Five/src/main.cpp
@@ -3,7 +3,17 @@
 using namespace std;
 
 int main() {
-    
+    vector<int> nums;
+    int num;
+    int middleNum;
+
+    while (cin >> num && num > 0) {
+       nums.push_back(num);
+    }
+
+    middleNum = nums.size() / 2;
+
+    cout << "Middle item: " << nums.at(middleNum) << endl;
 
     return 0;
 }
